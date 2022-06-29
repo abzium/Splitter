c8283fd53a82b89be8ec5a8280cfd87565f9dfe5
Abdul <50076007+abzium@users.noreply.github.com>
2022-06-29 12:37:38 -0400

    initial window

---
+++ Splitter.java
@@ -0,0 +1,29 @@
+// Splitter, a to-do list that organizes subtasks under main tasks and 
+// indicates progress based on completion of subtasks.
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Splitter extends JFrame {
+
+    public Splitter() {
+        Container cp = getContentPane();
+
+        cp.setLayout(new FlowLayout());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Splitter");
+        setSize(800, 600);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Splitter();
+            }
+        });
+    }
+}
