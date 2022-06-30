e1ca22a1d346fe10d2e14c01df71cd1b0a5be4da
Abdul <50076007+abzium@users.noreply.github.com>
2022-06-30 13:21:02 -0400

    better layout

--- Splitter.java
+++ Splitter.java
@@ -6,11 +6,32 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Splitter extends JFrame {
+    private JButton btnAdd, btnDelete, btnEdit;
+    private JList<String> taskList;
 
     public Splitter() {
-        Container cp = getContentPane();
+        // Create panel for buttons on top
+        JPanel pnlButtons = new JPanel(new GridLayout(1, 3, 5, 1));
 
-        cp.setLayout(new FlowLayout());
+        // Add buttons to panel
+        btnAdd = new JButton("Add");
+        pnlButtons.add(btnAdd);
+        btnDelete = new JButton("Delete");
+        pnlButtons.add(btnDelete);
+        btnEdit = new JButton("Edit");
+        pnlButtons.add(btnEdit);
+
+        // Create panel for list and add list to panel
+        JPanel pnlList = new JPanel(new GridLayout());
+        String[] data = {"one", "two", "three", "four"};
+        taskList = new JList<>(data);
+        pnlList.add(taskList);
+
+        // add panels to content pane
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout(3, 3));
+        cp.add(pnlButtons, BorderLayout.NORTH);
+        cp.add(pnlList, BorderLayout.CENTER);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Splitter");
